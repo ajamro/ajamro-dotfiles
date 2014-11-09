b8840ea32626093dd58a911c762958729775db0e
Arkadiusz Jamrocha <arkadiusz@jamrocha.net>
2014-11-09 14:41:19 +0100

    Adding first configuration files

---
+++ .vimrc
@@ -0,0 +1,12 @@
+" File ~/.vimrc
+
+filetype plugin on
+filetype plugin indent on
+
+" Global settings for all files (note: may be overwritten in ftplugin).
+
+set nocompatible
+set tabstop=2
+set shiftwidth=2
+set noexpandtab
+
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SRC=$(dirname $(readlink -f "$0"))
+DST=$HOME
+REL=$(perl -MFile::Spec -e 'print File::Spec->abs2rel("'$SRC'","'$DST'")')
+
+echo "installing dot files..."
+echo "source directory: $SRC"
+echo "target directory: $DST"
+echo
+
+pushd "$DST"
+for filename in $REL/.[a-z0-9A-Z]*
+do
+	# [ -e "$REL/$filename" ] && rm -R "$REL/$filename"
+	echo ln -sf "$filename" "."
+	ln -sf "$filename" "."
+done
+popd
